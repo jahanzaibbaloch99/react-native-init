8167f88f40efa3adb291527af37e764cc8be187c
devwaleed <waleed.baig@aciano.net>
2021-05-03 15:52:17 +0500

    Clean code and changed App.js to functional comp

--- App.js
+++ App.js
@@ -4,12 +4,10 @@ import { Provider as ReduxProvider } from "react-redux";
 import MainNavigator from './src/Navigation/MainNavigator';
 import store from './src/Stores';
 
-export default class App extends Component {
-	render() {
-		return (
-			<ReduxProvider store={store}>
-				<MainNavigator />
-			</ReduxProvider>
-		);
-	}
+export default function App(props) {
+	return (
+		<ReduxProvider store={store}>
+			<MainNavigator />
+		</ReduxProvider>
+	);
 }
--- index.js
+++ index.js
@@ -1,21 +1,5 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
-import handleDeeplinks from './src/Helpers/Deeplinks';
-import { initFCM, getToken } from "./src/Helpers/FCM";
-import { disableInAppMessages, enableInAppMessages } from "./src/Helpers/InAppMessaging";
-import { logEvent } from "./src/Helpers/FirebaseEvents";
-
-import defaultAppConfig from "./src/Config";
-import { initRemoteConfig } from "./src/Helpers/RemoteConfig";
-
-// initRemoteConfig(defaultAppConfig);
-// disableInAppMessages();
-// initFCM();
-// handleDeeplinks();
-// logEvent('appInit', {
-// 	awesome: true,
-// }).then(() => console.log('App Logged!'));
-// enableInAppMessages();
 
 AppRegistry.registerComponent(appName, () => App);
