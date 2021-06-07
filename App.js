5843ae571c2e04de1bdb969b355128ff11615fc7
Jahanzaib Ali <jhanzeb.ali99@gmail.com>
2021-06-07 17:04:03 +0500

    splash screen added

--- App.js
+++ App.js
@@ -3,8 +3,13 @@ import React, { Component } from 'react';
 import { Provider as ReduxProvider } from "react-redux";
 import MainNavigator from './src/Navigation/MainNavigator';
 import store from './src/Stores';
+import SplashScreen from 'react-native-splash-screen'
+
 
 export default function App(props) {
+	React.useEffect(() => {
+		SplashScreen.hide()
+	}, [])
 	return (
 		<ReduxProvider store={store}>
 			<MainNavigator />
